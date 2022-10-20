99908defb9f9a2afad8537ff3861d21dfc213608
NiaGoodwill <goodneg11@gmail.com>
2022-10-20 13:18:51 -0600

    wip

--- Game.cs
+++ Game.cs
@@ -1,13 +1,28 @@
 class Game 
 {
     Word word = new Word();
-    List<int> board = new List<int>();
+    List<int> wordWithSpaces = new List<int>();
     List<string> wordList;
     public Game ()
     {
         for (int i = 1; i <= 9; i++)
         {
-            board.Add (i);
+            wordWithSpaces.Add (i);
         } 
+    }
+
+    public void Print()
+    {
+        Console.WriteLine ("Testing");
+    }
+
+    public bool Check()
+    {
+        return false;
+    }
+
+    void AdjustWord ()
+    {
+
     }
 }
