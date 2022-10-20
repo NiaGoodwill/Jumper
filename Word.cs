d84d6d262aaaf557a8088be7054e4ef86154f663
NiaGoodwill <goodneg11@gmail.com>
2022-10-20 13:02:28 -0600

    word is done

--- Word.cs
+++ Word.cs
@@ -1,28 +1,33 @@
 class Word
 {
+    Random random = new Random();
     List<string> wordList = new List<string>();
-    List<string> theList = new List<string>();
+    List<string> charList = new List<string>();
+    string[] words = { "thunder", "quarter", "payment", "idea", "milk", "weather", "afterthought", "canvas", "island", "number", "rail", "snow", "dress", "smoke", "letter", "deer", "rub", "town", "tub", "relation", "juice", "vest", "reason", "houses", "regret", "knowledge", "eggnog", "eye", "fire", "spark", "cable", "robin", "tax", "pie", "quicksand", "airplane", "smile", "haircut", "kitty", "sand", "ants", "sky", "pancake", "territory", "plough", "box", "army", "edge", "crayon", "cook", "berry", "sail", "flowers", "playground", "brass", "mother", "locket", "view", "soda", "cow"};
     public Word()
     {
+        wordList.AddRange (words);
         string theWord = NewWord();
         CreateListOfChar(theWord);
     }
 
     string NewWord()
     {
-        return "hi";
+        int randIndex = random.Next(wordList.Count());
+        string newWord = wordList[randIndex];
+        return newWord;
     }
 
     void CreateListOfChar(string word)
     {
         foreach (char letter in word)
         {
-            theList.Add (letter.ToString());
+            charList.Add (letter.ToString());
         }
     }
 
     public List<string> GetWord()
     {
-        return theList;
+        return charList;
     }
 }
