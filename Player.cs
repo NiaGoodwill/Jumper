6b454cf2637c79d1f1618c547108191f0bf2ace6
Rachana Kumbhakar <rachanakumbhakar2019@gmail.com>
2022-10-20 12:47:51 -0600

    new update

--- Player.cs
+++ Player.cs
@@ -1,4 +1,16 @@
 class Player 
 {
-    
+    public string Guess(){
+       Console.WriteLine("Guess a letter [A-Z]:  ");
+       string letter = Console.ReadLine();
+       if (letter != null){
+        return letter;
+       }
+       else {
+        return "";
+       }
+       
+    }
+
 }
+    
