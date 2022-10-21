3e11047615998616e4c2f12b7a0bb6c7324ae15a
Rachana Kumbhakar <rachanakumbhakar2019@gmail.com>
2022-10-21 09:15:56 -0600

    done

--- Word.cs
+++ Word.cs
@@ -3,7 +3,12 @@ class Word
     Random random = new Random();
     List<string> wordList = new List<string>();
     List<string> charList = new List<string>();
-    string[] words = { "thunder", "quarter", "payment", "idea", "milk", "weather", "afterthought", "canvas", "island", "number", "rail", "snow", "dress", "smoke", "letter", "deer", "rub", "town", "tub", "relation", "juice", "vest", "reason", "houses", "regret", "knowledge", "eggnog", "eye", "fire", "spark", "cable", "robin", "tax", "pie", "quicksand", "airplane", "smile", "haircut", "kitty", "sand", "ants", "sky", "pancake", "territory", "plough", "box", "army", "edge", "crayon", "cook", "berry", "sail", "flowers", "playground", "brass", "mother", "locket", "view", "soda", "cow"};
+    string[] words = { "thunder", "quarter", "payment", "idea", "milk", "weather", 
+    "afterthought", "canvas", "island", "number", "rail", "snow", "dress", "smoke", "letter", 
+    "deer", "rub", "town", "tub", "relation", "juice", "vest", "reason", "houses", "regret",
+     "knowledge", "eggnog", "eye", "fire", "spark", "cable", "robin", "tax", "pie", "quicksand", "airplane", 
+     "smile", "haircut", "kitty", "sand", "ants", "sky", "pancake", "territory", "plough", "box", "army", "edge", 
+     "crayon", "cook", "berry", "sail", "flowers", "playground", "brass", "mother", "locket", "view", "soda", "cow"};
     public Word()
     {
         wordList.AddRange (words);
